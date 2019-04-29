d8f5eea5f757ec0adc687ae632367dcc09ce7c3f
Martin Richard <martius@martiusweb.net>
2019-04-29 10:57:53 +0200

    Bootstrap plugin

--- homefront/pelican/bootstrap.py
+++ homefront/pelican/bootstrap.py
@@ -1,62 +1,128 @@
 # coding: utf-8
 from typing import Type
 
+import glob
 import logging
+import os
 
 import pelican
+import pelican.generators
 import pelican.signals
 
-import homefront.pelican.saas
+import homefront
+import homefront.bootstrap
+import homefront.pelican.sass
 
 LOG = logging.getLogger(__name__)
 
 
-# TODO we probably don't need a generator, fiddling with saas settings is
-# likely sufficient if done early enough
-class BootstrapGenerator(homefront.pelican.saas.SaasGenerator):
-    def ensure_bootstrap_in_cache(self):
-        # raise NotImplementedError(self.settings["BOOTSTRAP_VERSION"])
-        pass
+class BootstrapPlugin:
+    _instance = None
 
-    def remove_bootstrap(self):
-        # remove from cache, let's use a cache/homefront/bootstrap_version to
-        # store the version, and re-install if not compat
-        pass
+    @classmethod
+    def get(cls):
+        """
+        Returns a singleton instance of :class:`BootstrapPlugin`.
 
-    def generate_output(self, writer):
-        self.ensure_bootstrap_in_cache()
-        super().generate_output(writer)
+        We use a singleton because Blinker uses weak references.
+        """
+        if not cls._instance:
+            cls._instance = cls()
+
+        return cls._instance
+
+    def __init__(self):
+        self.pelican = None
+        self.enabled = False
+        self.bootstrap_path = None
+
+        self.enable()
+
+    @property
+    def bootstrap_sass_path(self):
+        return os.path.join(self.bootstrap_path, "scss")
+
+    def enable(self) -> None:
+        pelican.signals.initialized.connect(self.initialize)
+        pelican.signals.get_generators.connect(self.get_generator)
+
+        self.update_include_path()
+
+        self.enabled = True
+
+    def disable(self) -> None:
+        self.enabled = False
+
+        self.update_include_path(enable=False)
+
+        pelican.signals.get_generators.disconnect(self.get_generator)
+        pelican.signals.get_generators.disconnect(self.initialize)
+
+    def update_include_path(self, enable: bool = True) -> None:
+        if not self.bootstrap_path:
+            return
+
+        if enable:
+            include_path = homefront.pelican.sass.SassGenerator.include_path
+            include_path.append(self.bootstrap_sass_path)
+        else:
+            include_path = homefront.pelican.sass.SassGenerator.include_path
+            include_path.remove(self.bootstrap_sass_path)
+
+    def initialize(self, pelican_object: pelican.Pelican) -> None:
+        self.pelican = pelican_object
+
+        try:
+            settings = pelican_object.settings
+            homefront.settings.update(settings)
+            self.bootstrap_path = self.ensure_bootstrap_ready(
+                settings["BOOTSTRAP_VERSION"])
+
+            LOG.debug("Bootstrap found at %s", self.bootstrap_path)
+
+            LOG.info("Enabling bootstrap %s", settings["BOOTSTRAP_VERSION"])
+            self.update_include_path()
+        except (homefront.Error, OSError):
+            LOG.exception("Failed to initialize bootstrap, plugin deactivated")
+            self.disable()
+
+    def get_generator(self, _: pelican.Pelican  # pylint: disable=R0201
+                      ) -> Type[pelican.generators.Generator]:
+        """
+        Return the class of the generator implemented by this plugin.
+        """
+        return homefront.pelican.sass.SassGenerator
+
+    def ensure_bootstrap_ready(self, version: str) -> None:
+        if not self.pelican:
+            raise homefront.Error("Plugin homefront.bootstrap did not receive "
+                                  "the initialized signal from pelican")
+
+        LOG.debug("Checking bootstrap %s", version)
+
+        cache_dir = homefront.settings.get_cache_dir(self.pelican.settings)
+        bootstrap_path = os.path.join(cache_dir, f"bootstrap-{version}")
+
+        # Bootstrap looks installed
+        if os.path.isdir(bootstrap_path):
+            return bootstrap_path
+
+        pattern = os.path.join(cache_dir, "bootstrap-*/")
+        for candidate in glob.iglob(pattern):
+            candidate_version = os.path.basename(candidate)
+            candidate_version = candidate_version.split("-", 1)[1]
+
+            if homefront.bootstrap.version_matches(
+                    version, candidate_version):
+                return candidate
+
+        # Could'nt find suitable version of bootstrap, let's install it
+        homefront.bootstrap.download_bootstrap(version, bootstrap_path)
+        return bootstrap_path
 
 
-def get_generators(_: pelican.Pelican) -> Type[BootstrapGenerator]:
+def register() -> None:
     """
-    Return the class of the generator implemented by this plugin.
+    Register this pelican plugin.
     """
-    return BootstrapGenerator
-
-
-def register():
-    """
-    Register pelican signal handlers.
-    """
-    pelican.signals.get_generators.connect(get_generators)
-
-
-"""
-TODO:
-* implement a new generator which generate sass files
-usually we've got a pelican object as a parameter of the generator
-check its content, there's probably the settings
-
-* maybe a homefront.settings.get(pelican, settingname) -> setting with possible
-override or default from homefront
-
-* add a bootstrap.lock in homefront cache with version
-
-* above functions better be in a class with the pelican object, no?
-
-
-
-====
-* how to log stuff with pelican in a plugin? >>>> logging cool
-"""
+    BootstrapPlugin.get()
