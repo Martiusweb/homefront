46d18f88a919d719b8ec813d5c387478b7e6572d
Martin Richard <martius@martiusweb.net>
2019-05-02 20:19:18 +0200

    bootstrap with deps

--- homefront/bootstrap.py
+++ homefront/bootstrap.py
@@ -8,31 +8,29 @@ import homefront
 import homefront.googleclosure
 import homefront.release
 
-_PACKAGE = "bootstrap"
 
-_ARCHIVE_DIRECTORIES_TO_EXTRACT = {
-    # scss files are under a bootstrap directory, so they can be included with
-    # "bootstrap/the_lib".
-    "package/scss/": "scss/bootstrap",
-    # js files are compressed and concatenated anyway.
-    "package/js/src/": "js/",
-}
-
-
-class Release(homefront.release.NpmRelease):
+class Bootstrap(homefront.release.NpmRelease):
     """
-    Represents a release to fetch from github.
+    Represents a release to fetch.
     """
     def __init__(self, required_version: str,
                  destination: Union[str, os.PathLike]):
-        super().__init__(required_version, _PACKAGE, destination,
-                         _ARCHIVE_DIRECTORIES_TO_EXTRACT)
-        self.name = "Bootstrap"
+        super().__init__(required_version, "bootstrap", destination, {
+            # scss files are under a bootstrap directory, so they can be
+            # included with "bootstrap/the_lib".
+            "package/scss/": "scss/bootstrap",
+            # js files are compressed and concatenated anyway.
+            "package/js/src/": "js/",
+            "package/package.json": "package.json"
+        })
 
 
 def download(version: str, destination: Union[str, os.PathLike]) -> None:
-    release = Release(version, destination)
+    release = Bootstrap(version, destination)
     release.install()
+
+    for dep in release.get_dependencies():
+        dep.install()
 
 
 @dataclasses.dataclass
@@ -42,6 +40,7 @@ class ArtifactWithBootstrap(homefront.googleclosure.Artifact):
     """
     bootstrap_modules: List[str] = dataclasses.field(default_factory=list)
     bootstrap_path: Union[str, os.PathLike] = ""
+    dependencies_path: Union[str, os.PathLike] = ""
 
     @classmethod
     def from_artifact(cls, artifact: homefront.googleclosure.Artifact
@@ -52,6 +51,10 @@ class ArtifactWithBootstrap(homefront.googleclosure.Artifact):
     def resolve_sources(self, basedir: Union[str, os.PathLike]):
         for module in self.bootstrap_modules:
             yield os.path.join(self.bootstrap_path, module + ".js")
+
+        # Add dependencies
+        yield os.path.join(self.dependencies_path, "**/package.json")
+        yield os.path.join(self.dependencies_path, "**.js")
 
         yield from super().resolve_sources(basedir)
 
