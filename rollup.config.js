0b930da4cfa04e4d0fef569df94361e90b11f320
Martin Richard <martius@martiusweb.net>
2019-05-04 17:53:23 +0200

    minify js

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,15 @@
 'use strict'
+// vim: ts=2 sw=2 et :
 
 const path = require('path')
 const resolve = require('rollup-plugin-node-resolve')
+
+import { terser } from 'rollup-plugin-terser';
 
 // List of ignored/external dependencies (not to be added in the final js)
 const external = []  // ex: ["popper.js"]
 const globals = {}  // ex: {"popper.js": "Popper"}
 
-const plugins = [
-  // resolve imports from node_modules
-  resolve({
-      browser: true
-  })
-]
 
 module.exports = {
   input: path.resolve(__dirname, 'theme/default/js/main.js'),
@@ -23,5 +20,14 @@ module.exports = {
     globals
   },
   external,
-  plugins
+  plugins: [
+    resolve({
+      browser: true
+    }),
+    terser({
+      sourcemap: true,
+      compress: true,
+      mangle: true
+    }),
+  ]
 }
